03f9fbacfb37e1e5bc6d0bc82507204dc22b23fd
adityayadav7 <adityay179@gmail.com>
2020-10-06 20:24:01 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <div className='page1'>
+     <iframe className='page' src="https://resume.creddle.io/embed/ipi4hkbimvt"
+  width="850" height="1100" seamless></iframe>
+  </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
